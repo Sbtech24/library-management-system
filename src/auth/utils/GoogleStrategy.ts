b624bafa0aea42a5b27c00cd2605985eddec521e
Sbtech24 <bajomosemilore@gmail.com>
2026-06-29 23:28:34 +0100

    Add @nestjs/config for environment variable management and update GoogleStrategy to use config values

--- src/auth/utils/GoogleStrategy.ts
+++ src/auth/utils/GoogleStrategy.ts
@@ -4,14 +4,16 @@ import { Profile } from "passport";
 import { Strategy } from "passport-google-oauth20";
 import { ConfigService } from "@nestjs/config";
 
+
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy){
     constructor(private readonly configService:ConfigService){
-      
+        console.log(configService.get('clientID'))
+   
         super({
-            clientID:configService.get('clientId')!,
-            clientSecret:configService.get('ClientSecret')!,
-            callbackURL:configService.get('callbackUrl')!,
+        clientID: configService.get<string>('clientID')!,
+      clientSecret: configService.get<string>('clientSecret')!,
+      callbackURL: configService.get<string>('callbackUrl')!,
             scope:['profile','email']
         })
 
